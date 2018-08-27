c4cbc77c67a15b744ff392678bd6c013948d188c
Poyan <Poyan.delavari@gmail.com>
2018-08-27 12:16:19 +0200

    a

---
+++ WindowsFormsApp1/WindowsFormsApp1/Home.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddB = new System.Windows.Forms.Button();
+            this.SearchB = new System.Windows.Forms.Button();
+            this.AllB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddB
+            // 
+            this.AddB.Location = new System.Drawing.Point(12, 12);
+            this.AddB.Name = "AddB";
+            this.AddB.Size = new System.Drawing.Size(158, 111);
+            this.AddB.TabIndex = 0;
+            this.AddB.Text = "add";
+            this.AddB.UseVisualStyleBackColor = true;
+            this.AddB.Click += new System.EventHandler(this.AddB_Click);
+            // 
+            // SearchB
+            // 
+            this.SearchB.Location = new System.Drawing.Point(176, 12);
+            this.SearchB.Name = "SearchB";
+            this.SearchB.Size = new System.Drawing.Size(158, 111);
+            this.SearchB.TabIndex = 2;
+            this.SearchB.Text = "search";
+            this.SearchB.UseVisualStyleBackColor = true;
+            // 
+            // AllB
+            // 
+            this.AllB.Location = new System.Drawing.Point(340, 12);
+            this.AllB.Name = "AllB";
+            this.AllB.Size = new System.Drawing.Size(158, 111);
+            this.AllB.TabIndex = 3;
+            this.AllB.Text = "all";
+            this.AllB.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 144);
+            this.Controls.Add(this.AllB);
+            this.Controls.Add(this.SearchB);
+            this.Controls.Add(this.AddB);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddB;
+        private System.Windows.Forms.Button SearchB;
+        private System.Windows.Forms.Button AllB;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/search.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextD = new System.Windows.Forms.Label();
+            this.TextS = new System.Windows.Forms.Label();
+            this.TextC = new System.Windows.Forms.Label();
+            this.TextL = new System.Windows.Forms.Label();
+            this.TextN = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.SearchBox.Location = new System.Drawing.Point(157, 21);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(265, 31);
+            this.SearchBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Search :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(27, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Domains :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Salary :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Comapny :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Last name :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Name :";
+            // 
+            // TextD
+            // 
+            this.TextD.AutoSize = true;
+            this.TextD.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextD.Location = new System.Drawing.Point(157, 265);
+            this.TextD.Name = "TextD";
+            this.TextD.Size = new System.Drawing.Size(68, 25);
+            this.TextD.TabIndex = 19;
+            this.TextD.Text = "XXXX";
+            // 
+            // TextS
+            // 
+            this.TextS.AutoSize = true;
+            this.TextS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextS.Location = new System.Drawing.Point(157, 221);
+            this.TextS.Name = "TextS";
+            this.TextS.Size = new System.Drawing.Size(68, 25);
+            this.TextS.TabIndex = 18;
+            this.TextS.Text = "XXXX";
+            // 
+            // TextC
+            // 
+            this.TextC.AutoSize = true;
+            this.TextC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextC.Location = new System.Drawing.Point(157, 178);
+            this.TextC.Name = "TextC";
+            this.TextC.Size = new System.Drawing.Size(68, 25);
+            this.TextC.TabIndex = 17;
+            this.TextC.Text = "XXXX";
+            // 
+            // TextL
+            // 
+            this.TextL.AutoSize = true;
+            this.TextL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextL.Location = new System.Drawing.Point(157, 137);
+            this.TextL.Name = "TextL";
+            this.TextL.Size = new System.Drawing.Size(68, 25);
+            this.TextL.TabIndex = 16;
+            this.TextL.Text = "XXXX";
+            // 
+            // TextN
+            // 
+            this.TextN.AutoSize = true;
+            this.TextN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextN.Location = new System.Drawing.Point(157, 91);
+            this.TextN.Name = "TextN";
+            this.TextN.Size = new System.Drawing.Size(68, 25);
+            this.TextN.TabIndex = 15;
+            this.TextN.Text = "XXXX";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 63);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Modify";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 63);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Add to list";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(428, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 31);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Enter";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 408);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextD);
+            this.Controls.Add(this.TextS);
+            this.Controls.Add(this.TextC);
+            this.Controls.Add(this.TextL);
+            this.Controls.Add(this.TextN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.label2);
+            this.Name = "search";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TextD;
+        private System.Windows.Forms.Label TextS;
+        private System.Windows.Forms.Label TextC;
+        private System.Windows.Forms.Label TextL;
+        private System.Windows.Forms.Label TextN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
